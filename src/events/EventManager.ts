29dbdd1bb04a7451e72b4fda03ddb32e894780f8
royaljacquesytb <63215754+royaljacques@users.noreply.github.com>
2023-05-29 00:55:14 +0200

    update code for compatible of eslint

--- src/events/EventManager.ts
+++ src/events/EventManager.ts
@@ -10,17 +10,10 @@ export default class EventManager {
 
   private async load() : Promise<void> {
     const files = readdirSync(`${__dirname}/list`).filter(file => file.endsWith(".ts") || file.endsWith(".js"));
-
-    let i = 0;
     for (const file of files) {
       const dynamicImport = await import(`./list/${file}`);
       const event: Event = new dynamicImport.default();
-
       Index.instance[event.once ? "once" : "on"](event.name, (...args) => event.execute(...args));
-      i++;
     }
-
-   
   }
-
 }
