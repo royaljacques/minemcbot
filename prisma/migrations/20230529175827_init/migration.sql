ad0969d079172f30b04117f9b774375e73c20042
royaljacquesytb <63215754+royaljacques@users.noreply.github.com>
2023-05-30 04:58:13 +0200

    remove migration

--- prisma/migrations/20230529175827_init/migration.sql
+++
@@ -1,9 +0,0 @@
--- CreateTable
-CREATE TABLE "Player" (
-    "id" SERIAL NOT NULL,
-    "name" TEXT NOT NULL,
-    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "updatedAt" TIMESTAMP(3) NOT NULL,
-
-    CONSTRAINT "Player_pkey" PRIMARY KEY ("id")
-);
