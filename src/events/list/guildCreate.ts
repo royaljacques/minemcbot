217d6fa595d3a54b196da3f8b6875946d0b5b186
royaljacquesytb <63215754+royaljacques@users.noreply.github.com>
2023-06-08 00:59:41 +0200

    patch server member count

--- src/events/list/guildCreate.ts
+++ src/events/list/guildCreate.ts
@@ -5,14 +5,11 @@ import Index from "../..";
 export default class Ready extends Event {
     public readonly name = "guildCreate";
     async execute(guild: Guild) : Promise<void> {
-     
-      
       guild.leave();
-      const members = guild.members.cache.filter((member: GuildMember )=> !member.user.bot).size;
+      const members = (await (guild.members.fetch())).filter(member => !member.user.bot).size;
       const image = await generateImage((await guild.fetchOwner()).user.username,guild.name, members);
       const channel = await Index.instance.channels.fetch("1115606253603926046") as TextChannel
-      channel.send({files: [{ attachment: image, name: 'newServer.png' }]});
-      
+      channel.send({files: [{ attachment: image, name: 'newServer.png' }]});    
     }
 }
 
