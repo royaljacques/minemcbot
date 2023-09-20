1e8d437944d005c736ba31a5c92586bc54261621
royaljacquesytb <maximejacques667@gmail.com>
2023-09-20 21:42:15 +0200

    init modal for register new user

--- src/commands/list/start.ts
+++ src/commands/list/start.ts
@@ -1,6 +1,7 @@
 import BaseCommand from "../baseCommands";
-import { ChatInputCommandInteraction, SlashCommandBuilder } from "discord.js";
+import { ActionRowBuilder, ChatInputCommandInteraction, ModalActionRowComponentBuilder, ModalBuilder, SlashCommandBuilder, TextInputBuilder, TextInputStyle } from "discord.js";
 import { CommandsType } from "../baseCommands";
+import { prisma } from "../../minemc";
 
 export default class Ping extends BaseCommand {
 
@@ -14,7 +15,35 @@ export default class Ping extends BaseCommand {
     "category": CommandsType.GAMEPLAY
   }
   public async execute(command: ChatInputCommandInteraction): Promise<void> {
-    await command.reply("pong");
+
+    const collectorFilter = (i: any) => {
+      i.deferUpdate();
+      return i.user.id === command.user.id;
+    };
+   
+    prisma.player.findUnique({where: {discordId: command.user.id.toString()}}).then(async (player) => {
+      if(player === null){
+        const modal: ModalBuilder = new ModalBuilder()
+        .setCustomId("start:modal")
+        .setTitle("start")
+
+        const pseudo = new TextInputBuilder()
+          .setCustomId("start:pseudo")
+          .setPlaceholder("pseudo")
+          .setLabel("what is your pseudo ?")
+          .setStyle(TextInputStyle.Short)
+        const row = new ActionRowBuilder<ModalActionRowComponentBuilder>().addComponents(pseudo);
+        modal.addComponents(row);
+        await command.showModal(modal).then(async() => {
+         command.awaitModalSubmit({time: 60000, filter: collectorFilter}).then(async (modalCollector) => {
+          command.channel?.send("you are ready to start the adventure `" + modalCollector.fields.getTextInputValue("start:pseudo") + "`");
+          console.log();
+         });
+        });
+       }else{
+        await command.reply("you have already start the adventure");
+       }
+    });
   }
 }
 /**
