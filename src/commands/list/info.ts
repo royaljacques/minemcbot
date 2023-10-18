002b3eed8651cdc132c7919ff1d28dea25d2d32b
royaljacquesytb <maximejacques667@gmail.com>
2023-10-18 21:31:16 +0200

    finalise /info commands

--- src/commands/list/info.ts
+++ src/commands/list/info.ts
@@ -17,8 +17,29 @@ export default class Ping extends BaseCommand {
   }
   public async execute(command: ChatInputCommandInteraction) : Promise<void> {
     console.log("info")
-    const profil = Index.instance.getProfils().getUser(command.user.id);
-    console.log(profil)
+    const user = Index.instance.getProfils().getUser(command.user.id);
+    if(user === null || user === undefined){
+      command.reply({content: "you don't have use /start command", ephemeral: true})
+    }else{
+      const guilds = await command.client.guilds.fetch();
+      //get all users
+      const guildsSize = guilds.size;
+      const memberCount = Index.instance.guilds.cache.map((guild: Guild) => guild.memberCount).reduce((a, b) => a + b, 0);
+      const embed = new EmbedBuilder()
+      .setTitle(user === null ? getLanguage("info_embed_title", "en"): getLanguage("info_embed_title", user.language))
+      .addFields(
+        {name: getLanguage("info_embed_field_creator", user === null ? "EN_en": user.language), value: "<@883693434693619732>", inline: true},
+        {name: getLanguage("info_embed_field_total_guilds", user === null ? "EN_en": user.language), value: guildsSize.toString(), inline: true},
+        {name: getLanguage("info_embed_field_total_users", user === null ? "EN_en": user.language), value: memberCount.toString(), inline: true},
+        {name: getLanguage("info_embed_field_version", user === null ? "EN_en": user.language), value: "0.0.1", inline: true},
+        {name: getLanguage("info_embed_field_dev_language", user === null ? "EN_en": user.language), value: "TypeScript", inline: true},
+        {name: getLanguage("info_embed_field_api", user === null ? "EN_en": user.language), value: "discord.js", inline: true},
+        {name: getLanguage("info_embed_field_discordserver", user === null ? "EN_en": user.language), value: "[click](https://discord.gg/HJJrq5d5eU)", inline: true},
+        {name: getLanguage("info_embed_field_uptime", user === null ? "EN_en": user.language), value: "<t:"+Index.startBotTime+":R>", inline: true},
+        {name: getLanguage("info_embed_field_invite", user === null ? "EN_en": user.language), value: "[click](https://top.gg/fr/bot/1112497189458038907)", inline: true},
+      );
+      command.reply({embeds: [embed]})
+    }
   }
 }
 
